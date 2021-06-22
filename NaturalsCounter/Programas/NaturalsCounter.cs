b7184844721e06786900c6c6944698188f849232
WilmerCM01 <84152101+WilmerCM01@users.noreply.github.com>
2021-06-22 18:11:32 -0400

    Update NaturalsCounter.cs

--- NaturalsCounter/Programas/NaturalsCounter.cs
+++ NaturalsCounter/Programas/NaturalsCounter.cs
@@ -7,27 +7,27 @@ namespace Programas
     {
         private static void Main(string[] args){
 
-			Console.WriteLine("Contador de números naturales del 1 al 300");
-			Console.WriteLine();
+		Console.WriteLine("Contador de números naturales del 1 al 300");
+		Console.WriteLine();
 			
-			foreach(int numero in NumerosNaturales(300)){
+		foreach(int numero in NumerosNaturales(300)){
 			
-				Console.WriteLine(numero);
+			Console.WriteLine(numero);
 				
-			}
-
-        }
-	
-		public static IEnumerable<int> NumerosNaturales(int maximo){
-		
-			for(int i = 1; i <= maximo; i++){
-			
-				yield return i;
-				
-			}
-			
-			yield break;
-		
 		}
+
+      	}
+	
+	public static IEnumerable<int> NumerosNaturales(int maximo){
+		
+		for(int i = 1; i <= maximo; i++){
+			
+			yield return i;
+				
+		}
+			
+		yield break;
+		
+	}
     }
 }
