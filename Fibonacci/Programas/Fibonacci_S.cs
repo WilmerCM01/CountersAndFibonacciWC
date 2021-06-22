f8176ff4e3cb547cc797a045dcf3fc756ef431d7
WilmerCM01 <84152101+WilmerCM01@users.noreply.github.com>
2021-06-22 18:04:27 -0400

    Add files via upload

---
+++ Fibonacci/Programas/Fibonacci_S.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Programas
+{
+    class Fibonacci_S
+    {
+        static void Main(string[] args){
+
+			Console.WriteLine("Secuencia Fibonacci");
+			Console.WriteLine();
+			
+			foreach(int numero in Fibonacci(20)){
+			
+				Console.WriteLine(numero);
+				
+			}
+
+        }
+	
+		public static IEnumerable<int> Fibonacci(int x){
+			
+    		int num1 = -1;
+    		int num2 = 1;
+			
+    		for (int i = 0; i < x; i++){
+				
+       			int suma = num1 + num2;
+       			num1 = num2;
+       			num2 = suma;
+       			yield return suma;
+    		}
+			
+			yield break;
+		}
+    }
+}
