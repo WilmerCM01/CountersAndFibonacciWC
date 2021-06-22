1e6dde9af16e7154d12326ee04a593f5e648d3cb
WilmerCM01 <84152101+WilmerCM01@users.noreply.github.com>
2021-06-22 18:14:12 -0400

    Update PrimesCounter.cs

--- PrimesCounter/Programas/PrimesCounter.cs
+++ PrimesCounter/Programas/PrimesCounter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Programas
@@ -7,43 +7,43 @@ namespace Programas
     {
         static void Main(string[] args){
 
-			Console.WriteLine("Contador de números primos");
-			Console.WriteLine();
+		Console.WriteLine("Contador de números primos");
+		Console.WriteLine();
 			
-			foreach(int numero in Primos(100)){
+		foreach(int numero in Primos(100)){
 			
-				Console.WriteLine(numero);
+			Console.WriteLine(numero);
 				
-			}
+		}
 
-        }
+       	}
 	
-		public static IEnumerable<int> Primos(int x){
+	public static IEnumerable<int> Primos(int x){
 			
-			Boolean verificador = true;
-			int a, b;
+		Boolean verificador = true;
+		int a, b;
 			
-        	for (a = 2; a <= x; a++) {
+        	for (a = 2; a <= x; a++){
 				
-            	for (b = 2; b <= x; b++) {
+            		for (b = 2; b <= x; b++){
 					
-                	if (a != b && a % b == 0) {
+                		if (a != b && a % b == 0){
 						
-                    	verificador = false;
-                    	break;
-					}  
+                    			verificador = false;
+                    			break;
+				}  
            		 }
 				
-				if(verificador){
+			if(verificador){
 			
-					yield return a;
+				yield return a;
 				
-				}
-				
-				verificador = true;
 			}
-			
-			yield break;
+				
+			verificador = true;
 		}
+			
+		yield break;
+	}
     }
 }
